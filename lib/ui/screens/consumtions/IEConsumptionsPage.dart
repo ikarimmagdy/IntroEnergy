33388f744c684e49084c10d6a030056cb48101c1
ikarimmagdy <ikarimmagdy@gmail.com>
2020-10-11 16:39:44 +0400

    Add Consumption Page

--- lib/ui/screens/consumtions/IEConsumptionsPage.dart
+++ lib/ui/screens/consumtions/IEConsumptionsPage.dart
@@ -6,7 +6,7 @@ class IEConsumptionsPage extends StatefulWidget {
 
   @override
   State<StatefulWidget> createState() {
-   return _IEConsumptionsState();
+    return _IEConsumptionsState();
   }
 }
 
@@ -18,6 +18,196 @@ class _IEConsumptionsState extends State<IEConsumptionsPage> {
           title: Text("Consumptions"),
         ),
         drawer: IESideMenu(),
-        body: Center(child: Text("Events")));
+        body: Padding(padding: EdgeInsets.all(10), child: _createList()));
   }
+
+  Widget _createDataTable() {
+    return DataTable(columns: [
+      DataColumn(label: Text("")),
+      DataColumn(label: Text("")),
+      DataColumn(label: Text("")),
+    ], rows: [
+      DataRow(cells: [DataCell(Text("sdsdsd"))])
+    ]);
+  }
+}
+
+Widget _createList() {
+  return Column(
+    children: [
+      _createTitle("Meter Info"),
+      _createMeterInfo(),
+      ListView.builder(
+      shrinkWrap: true,
+      itemCount: 3,
+      itemBuilder: (context, index) {
+        return _createCard();
+      },
+    )],
+  );
+}
+
+Widget _createCard() {
+  return Card(
+    child: Padding(
+      padding: EdgeInsets.all(10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+          Row(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                Text("Start: ",style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black,
+                    fontSize: 15),
+                ),
+                Text("Cost:",style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black,
+                    fontSize: 15)),
+                Text("Month:"),
+              ],),
+              Padding(
+                padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                  Text("0 KWh ",style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      color: Colors.blueAccent,
+                      fontSize: 15),
+                  ),
+                  Text("534.89 KWh ",style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      color: Colors.green,
+                      fontSize: 15)),
+                  Text("June 2020"),
+                ],),
+              )
+            ],
+          ),
+          Text("View invoice",style: TextStyle(
+              fontWeight: FontWeight.normal,
+              color: Colors.black12,
+              fontSize: 15),
+          ),
+
+        ],),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Current: ",style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        color: Colors.black,
+                        fontSize: 15),
+                    ),
+                    Text("Cons.:",style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        color: Colors.black,
+                        fontSize: 15)),
+                    Text("Reading:"),
+                  ],),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("534 KWh ",style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          color: Colors.red,
+                          fontSize: 15),
+                      ),
+                      Text("534.89 EGP ",style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          color: Colors.blueAccent,
+                          fontSize: 15)),
+                      Text("30-06-2020"),
+                    ],),
+                )
+              ],
+            ),
+          Text("Download",style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.lightGreen,
+              fontSize: 15),
+          ),
+        ],)
+      ],),
+    )
+  );
+}
+
+Widget _createMeterInfo() {
+  return Padding(
+      padding: EdgeInsets.fromLTRB(15, 15, 15, 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Flexible(
+            flex: 1,
+            child: Column(
+              children: [
+                Padding(
+                  padding:EdgeInsets.fromLTRB(0, 0, 10, 0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [Icon(Icons.arrow_back), Text("12223"), Text("4222256")],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding:EdgeInsets.fromLTRB(10, 0, 0, 0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [Text("Fllor 1"), Icon(Icons.arrow_forward)],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ));
+}
+
+Widget _createTitle(String title) {
+  return Container(
+    color: Colors.black12,
+    child: Padding(
+      padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
+      child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+        Icon(Icons.payment),
+        Text(
+          title,
+          style: TextStyle(
+              fontWeight: FontWeight.normal,
+              color: Colors.black,
+              fontSize: 15),
+        ),
+      ]),
+    ),
+  );
 }
